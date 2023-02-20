0243bd1f1e48a88a66a4177f30962c60fd50a818
jitinknoldus <jitin.saxena@knoldus.com>
2023-02-20 19:45:11 +0530

    jenkins-assignment

---
+++ jenkinsAssignment.java
@@ -0,0 +1,7 @@
+//jenkins assignment
+
+public class JenkinsAssignment {
+    public static void main(String args[]) {
+        System.out.println("Hello Mentor! This is my Jenkins Assignemnt");
+    }
+}
